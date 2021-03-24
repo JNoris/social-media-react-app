a3f5c88fe39718deff70aa81b97cdb4b892fd10b
Noris <45073527+JNoris@users.noreply.github.com>
2021-03-23 18:16:32 -0600

    Settings Skeleton Complete
    Took my a lot longer than expected because search bars are more confusing than I was expecting.
    
    Dark theme and router are not properly implemented.

--- social-media-react-app/src/App.js
+++ social-media-react-app/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Route } from "react-router";
 import { Layout } from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
@@ -18,6 +19,31 @@ function App() {
     <div className="App">
       <SignInSide />
     </div>
+=======
+import { Route, Router, Switch } from "react-router";
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile";
+import SignUp from "./components/Login/SignUp";
+import Settings from "./components/Settings/Settings";
+import SignInSide from "./components/Login/SignInSide";
+import SideNav from "./components/SideNav/SideNav";
+import { Flex } from "./App.styles";
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignInSide} />
+        <Route exact path="/settings" component={Settings} />
+        <Flex>
+          <SideNav />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/profile" component={Profile} />
+        </Flex>
+      </Switch>
+    </>
+>>>>>>> Stashed changes
   );
 }
 
