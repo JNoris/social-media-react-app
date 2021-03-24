fe7b36e37cd37f10bbdc8d452f1dcecdf0e28254
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2021-03-24 11:40:53 -0600

    fixed

--- social-media-react-app/src/App.js
+++ social-media-react-app/src/App.js
@@ -5,6 +5,7 @@ import SignUp from './components/Login/SignUp';
 import SignInSide from './components/Login/SignInSide';
 import SideNav from './components/SideNav/SideNav';
 import { Flex } from './App.styles';
+import Settings from './components/Settings/Settings';
 import Follow from './components/Follow/Follow';
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <SideNav />
           <Route exact path="/" component={Home} />
           <Route exact path="/profile" component={Profile} />
+          <Route exact path="/follow" component={Follow}/>
         </Flex>
       </Switch>
     </>
