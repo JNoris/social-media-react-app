abff4cb427eb00cb131e20bebe86ba1912636662
Noris <45073527+JNoris@users.noreply.github.com>
2021-03-24 17:23:02 -0600

    TopNav + Chat partially done.
    Nearly complete. Still need to sort out stuff.

--- social-media-react-app/src/App.js
+++ social-media-react-app/src/App.js
@@ -5,15 +5,18 @@ import SignUp from "./components/Login/SignUp";
 import Settings from "./components/Settings/Settings";
 import SignInSide from "./components/Login/SignInSide";
 import SideNav from "./components/SideNav/SideNav";
+import TopNav from "./components/TopNav/TopNav";
 import { Flex } from "./App.styles";
 
 function App() {
   return (
     <>
+      <TopNav />
       <Switch>
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/signin" component={SignInSide} />
         <Route exact path="/settings" component={Settings} />
+
         <Flex>
           <SideNav />
           <Route exact path="/" component={Home} />
