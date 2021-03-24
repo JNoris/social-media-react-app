2c95c45788458d05940b78896cb33fa4d2e43df7
Athena <athenakozak@gmail.com>
2021-03-24 12:15:32 -0700

    added basic styling to comments

--- social-media-react-app/src/components/PostComment/PostComment.styles.js
+++ social-media-react-app/src/components/PostComment/PostComment.styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
 export const CommentWrapper = styled.div`
+    p{
+        color: #fff;
+        font-size: 1rem;
+    }
 
+.MuiListItem-root {
+    color: #c3c3c3;
+}
 `;
--- social-media-react-app/src/components/PostComment/PostCommentListView.js
+++ social-media-react-app/src/components/PostComment/PostCommentListView.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import {CommentWrapper} from './PostComment.styles'
 
 const PostCommentListView = (props) => {
     // TODO API call to get all post comments
@@ -44,15 +44,18 @@ const PostCommentListView = (props) => {
     return (
         <List>
             {tempComments.map((comment) => (
+                <CommentWrapper>
                   <ListItem key = {comment.username} alignItems="flex-start">
                   <ListItemAvatar>
                       <Avatar aria-label="user">RU</Avatar>
                   </ListItemAvatar>
                   <ListItemText
+                      id="listItem"
                       primary={comment.username}
                       secondary={comment.comment}
                   />      
               </ListItem>
+              </CommentWrapper>
             ))}
         </List>
     )
