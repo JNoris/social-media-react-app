940c1d38dae88214571462f38d7cb586c8b6585d
snowman00000 <thesnowman00000@gmail.com>
2021-03-25 13:38:39 -0400

    add firstname and lastname to applicationuser

---
+++ CapstoneIG_v1/Migrations/20210325173710_add firstname and lastname.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CapstoneIG_v1.Migrations
+{
+    public partial class addfirstnameandlastname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "ApplicationUser",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "ApplicationUser",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "ApplicationUser");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "ApplicationUser");
+        }
+    }
+}
