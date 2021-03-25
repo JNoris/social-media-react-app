a30eb20d9e0cee2f6762d78f4dceddbcafeab9d0
snowman00000 <thesnowman00000@gmail.com>
2021-03-25 15:05:30 -0400

    update sample data

---
+++ CapstoneIG_v1/Migrations/20210325184923_remove text limits.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CapstoneIG_v1.Migrations
+{
+    public partial class removetextlimits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CommentText",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(26)",
+                oldMaxLength: 26,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CommentText",
+                table: "Comments",
+                type: "nvarchar(26)",
+                maxLength: 26,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
