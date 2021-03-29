4ee251951f7ed88412a8ffeab529cb379f6b0c45
Noris <45073527+JNoris@users.noreply.github.com>
2021-03-29 11:15:32 -0600

    Delete SignUp.js
    Removed SignUp.js due to me breaking it.

--- social-media-react-app/src/components/Profile/Profile.js
+++ social-media-react-app/src/components/Profile/Profile.js
@@ -20,57 +20,6 @@ import SearchBar from "../TopNav/TopNavComponents/SearchBar";
 // Next line is for JWT validation
 // import AuthService from "../services/auth.service";
 
-/**
- * This page gets current User from Local Storage by calling AuthService.getCurrentUser() method
- * and show user information (with token).
- */
-
-/**
-// Begin validation code 
-export default class Profile extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: AuthService.getCurrentUser()
-    };
-  }
-
-  render() {
-    const { currentUser } = this.state;
-
-    return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>
-            <strong>{currentUser.username}</strong> Profile
-          </h3>
-        </header>
-        <p>
-          <strong>Token:</strong>{" "}
-          {currentUser.accessToken.substring(0, 20)} ...{" "}
-          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
-        <p>
-          <strong>Id:</strong>{" "}
-          {currentUser.id}
-        </p>
-        <p>
-          <strong>Email:</strong>{" "}
-          {currentUser.email}
-        </p>
-        <strong>Authorities:</strong>
-        <ul>
-          {currentUser.roles &&
-            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-        </ul>
-      </div>
-    );
-  }
-}  
-// End validation code
- */
-
 const Profile = (props) => {
   if ("userId" in props) {
     console.log(true);
