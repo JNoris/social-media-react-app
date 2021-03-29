97c8724b2cd50804b277ffabfc4809bd913bc90c
Athena <athenakozak@gmail.com>
2021-03-29 15:54:32 -0700

    added *blind* api calls to view comments / post

--- social-media-react-app/src/components/PostViewModal/PostCardModalView/PostCardModalView.js
+++ social-media-react-app/src/components/PostViewModal/PostCardModalView/PostCardModalView.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Card from '@material-ui/core/Card';
 import {CardWrapper} from '../../PostCard/PostCard.styles'
 import PostImage from '../../PostCard/PostCardContent/PostImage'
@@ -12,8 +13,7 @@ const PostCardModalView = (props) => {
 
     const post = props.post;
     const viewComments = props.viewComments
-
-    // API to get post description + comments
+ 
     var postDataToShow = viewComments ? (
         <PostCardModalContent 
             username={props.userName} 
@@ -26,7 +26,7 @@ const PostCardModalView = (props) => {
             username={props.userName} 
             userProfilePhoto={props.userProfilePhoto}
             //TODO call for posts
-            comments = {post.comments}
+            //comments = {comments}
             post={post}
                />
     ) ;
--- social-media-react-app/src/components/PostViewModal/PostComment/PostCommentList.js
+++ social-media-react-app/src/components/PostViewModal/PostComment/PostCommentList.js
@@ -1,42 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import List from '@material-ui/core/List';
 import {TitleWrapper} from './PostComment.styles'
 import PostCommentItem from './PostCommentItem'
 
 const PostCommentListView = (props) => {
     // TODO API call to get all post comments
+    const post = props.post;
+    const [comments, setComments] = useState([]);
+
+
+        axios.defaults.headers={
+            "Content-Type":"application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+
+        useEffect(()=>{
+            axios.get("https://localhost:5001/GetPostComments/" + post.Id)
+            .then(res => setComments(res.data))
+            .catch(err=>console.log(err))
+        },[])
 
     // temp dummy data
-    var tempComments = [
-        {
-            username: "bobbypark123",
-            comment: "Nice shot!"
-        }, 
-        {
-            username: "johhny_doe",
-            comment: "Wow"
-        }, 
-        {
-            username: "smithyjane93",
-            comment: "So pretty!"
-        }, 
-        {
-            username: "bugs_everywhere",
-            comment: "some comment"
-        }, 
-        {
-            username: "testing-scrolll",
-            comment: "some comment"
-        }, 
-        {
-            username: "more-scroll",
-            comment: "some comment"
-        }, 
-        {
-            username: "wut",
-            comment: "some comment"
-        }, 
-    ];
+    // var tempComments = [
+    //     {
+    //         username: "bobbypark123",
+    //         comment: "Nice shot!"
+    //     }, 
+    //     {
+    //         username: "johhny_doe",
+    //         comment: "Wow"
+    //     }, 
+    //     {
+    //         username: "smithyjane93",
+    //         comment: "So pretty!"
+    //     }, 
+    //     {
+    //         username: "bugs_everywhere",
+    //         comment: "some comment"
+    //     }, 
+    //     {
+    //         username: "testing-scrolll",
+    //         comment: "some comment"
+    //     }, 
+    //     {
+    //         username: "more-scroll",
+    //         comment: "some comment"
+    //     }, 
+    //     {
+    //         username: "wut",
+    //         comment: "some comment"
+    //     }, 
+    // ];
 
     return (
         <div>
@@ -44,7 +59,7 @@ const PostCommentListView = (props) => {
         <h6>Comments</h6>
         </TitleWrapper>
         <List>
-            {tempComments.map((comment) => (
+            {comments?.map((comment) => (
                <PostCommentItem comment={comment}/>
             ))}
         </List>
