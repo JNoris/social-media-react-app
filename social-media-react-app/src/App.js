5a94387fde7ab53c06fe75cecabd8bec36c303f7
Noris <45073527+JNoris@users.noreply.github.com>
2021-03-29 15:48:53 -0600

    Hooks Validation + Button Pressing
    I'd like to thank Edvin for this.

--- social-media-react-app/src/App.js
+++ social-media-react-app/src/App.js
@@ -2,17 +2,15 @@ import { BrowserRouter, Route, Router, Switch } from "react-router";
 import React, { Component } from "react";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
-import SignUp from "./components/Login/SignUp";
 import Settings from "./components/Settings/Settings";
-import SignInSide from "./components/Login/SignInSide";
 import SideNav from "./components/SideNav/SideNav";
 // import TopNav from "./components/TopNav/TopNav";
 import Chat from "./components/Chat/Chat";
 import { Flex, MainWrapper } from "./App.styles";
 import Follow from "./components/Follow/Follow";
 import AddPost from "./components/AddPost/AddPost";
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
+import Register from "./components/Login/Register";
+import Login from "./components/Login/Login";
 // import TopNav3 from "./components/Topnav/TopNav3";
 
 /**
@@ -40,10 +38,8 @@ function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/signin" component={SignInSide} />
         <Route exact path="/register" component={Register} />
-        <Route path="/login" component={Login} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="*" component={Register} />
         <Flex>
           <SideNav />
--- social-media-react-app/src/components/Login/Register.js
+++ social-media-react-app/src/components/Login/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import axios from 'axios';
-import styled from 'styled-components';
+import axios from "axios";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   position: fixed;
@@ -11,54 +11,111 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 
-  p{
-      display: block;
+  p {
+    display: block;
   }
 `;
 
 class Register extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    userName: "",
+    passWord: "",
+  };
 
-    state = {
-        firstName: '',
-        lastName: '',
-        userName: '',
-        passWord: ''
+  inputValidation = () => {
+    let res = false;
+
+    // First name, upper or lower case
+    let firstNameRegex = /^[a-zA-Z]+$/;
+    // Last name, upper or lower case
+    let lastNameRegex = /^[a-zA-Z]+$/;
+    // Username must be 3-16 characters
+    let usernameRegex = /^[a-z0-9_-]{3,16}$/;
+    // Password, 6-20 char and must have a number and a special character
+    let passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
+
+    //   this.state.userName === usernameRegex &&
+    //   this.state.passWord === passwordRegex
+    if (
+      this.state.userName != "" ||
+      usernameRegex.test(String(this.state.userName).toLowerCase())
+    ) {
+      res = true;
     }
 
-    handleRegisterButton = () => {
+    return res;
+  };
 
-        axios.post(`https://localhost:5001/register`, {
-    
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            userName: this.state.userName,
-            passWord: this.state.passWord,
-    
-        }).then(response=>{
-            if(response.status === 200)
-            {
-                this.props.history.push('/login')
-            }        
-        });     
+  handleRegisterButton = () => {
+    axios
+      .post(`https://localhost:5001/register`, {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        userName: this.state.userName,
+        passWord: this.state.passWord,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          this.props.history.push("/login");
+        }
+      });
+  };
+
+  handleKeypress = (e) => {
+    if (e.keyCode === 13 || e.charCode === 13) {
+      this.btn.click();
+    } else {
+      console.log("");
     }
+  };
 
+  render() {
+    return (
+      <Wrapper>
+        <p>First Name</p>
+        <input
+          type="text"
+          value={this.state.firstName}
+          onChange={(e) => {
+            this.setState({ firstName: e.target.value });
+          }}
+        />
+        <p>Last Name</p>
+        <input
+          type="text"
+          value={this.state.lastName}
+          onChange={(e) => {
+            this.setState({ lastName: e.target.value });
+          }}
+        />
+        <p>Username</p>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={(e) => {
+            this.setState({ userName: e.target.value });
+          }}
+        />
+        <p>Password</p>
+        <input
+          type="text"
+          value={this.state.passWord}
+          onChange={(e) => {
+            this.setState({ passWord: e.target.value });
+          }}
+        />
 
-    render(){
-        return(
-            <Wrapper>
-                <p>First Name</p>
-                <input type="text" value={this.state.firstName} onChange={(e)=>{this.setState({firstName: e.target.value})}}/>
-                <p>Last Name</p>
-                <input type="text" value={this.state.lastName} onChange={(e)=>{this.setState({lastName: e.target.value})}}/>
-                <p>Username</p>
-                <input type="text" value={this.state.userName} onChange={(e)=>{this.setState({userName: e.target.value})}}/>
-                <p>Password</p>
-                <input type="text" value={this.state.passWord} onChange={(e)=>{this.setState({passWord: e.target.value})}}/>
-
-                <input style={{marginTop: "1vh"}} type="button" onClick={this.handleRegisterButton} value="Register"/>
-            </Wrapper>
-        );
-    }
+        <input
+          style={{ marginTop: "1vh" }}
+          type="button"
+          onClick={this.handleRegisterButton}
+          value="Register"
+        />
+      </Wrapper>
+    );
+  }
 }
 
 export default Register;
