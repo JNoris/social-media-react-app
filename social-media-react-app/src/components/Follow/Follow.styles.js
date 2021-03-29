491ce3918bd24b0db7098716d751bf9197d8869a
Athena <athenakozak@gmail.com>
2021-03-29 09:26:58 -0700

    updated styling on follower/following views

--- social-media-react-app/src/components/Follow/Follow.styles.js
+++ social-media-react-app/src/components/Follow/Follow.styles.js
@@ -11,15 +11,15 @@ export const FollowWrapper = styled.div`
         color:white;
     }
 
-    .PrivateTabIndicator-colorSecondary-1,
-    .PrivateTabIndicator-colorSecondary-3,
-    .PrivateTabIndicator-colorSecondary-7,
-    .PrivateTabIndicator-colorSecondary-9,
-    .PrivateTabIndicator-colorSecondary-11,
-    .PrivateTabIndicator-colorSecondary-13,
-    .PrivateTabIndicator-colorSecondary-15 {
-        background-color: #fcac56
-    }
+    // .PrivateTabIndicator-colorSecondary-,
+    // .PrivateTabIndicator-colorSecondary-3,
+    // .PrivateTabIndicator-colorSecondary-7,
+    // .PrivateTabIndicator-colorSecondary-9,
+    // .PrivateTabIndicator-colorSecondary-11,
+    // .PrivateTabIndicator-colorSecondary-13,
+    // .PrivateTabIndicator-colorSecondary-15 {
+    //     background-color: #fcac56
+    // }
 
 `;
 
@@ -28,17 +28,42 @@ export const Flex = styled.div`
     padding: 0 2rem ;
     justify-content: space-between;
     align-items: center;
-    border: 1px solid black;
-    background-color:grey;
-    box-shadow: 5px 5px 10px 5px #000000;
-    margin-bottom:0.7rem;
-    border-radius:1rem;
+    //border: 1px solid black;
+    //background-color:grey;
+    background-color: rgba(255,255,255,.1);
+    //box-shadow: 5px 5px 10px 5px #000000;
+    //margin-bottom:0.7rem;
+    margin-bottom: 1rem;
+    //border-radius:1rem;
+    border-radius: 10px;
     button{
         width:20vw;
     }
     p{
         font-weight:bold;
     }
+
+
+    .MuiButton-outlined {
+        border: 1px solid rgba(48,53, 64,.7);
+    }
+
+    .MuiButton-outlined:hover {
+        border: 1px solid #e2336b;
+    }
+`;
+
+export const Wrapper = styled.div`
+    p {
+        color: #e3e3e3;
+    }
+
+    .subheader {
+        margin: 2rem 0;
+    }
+
+    max-width: 1000px;
+    margin: 0 auto;
 `;
 
 
