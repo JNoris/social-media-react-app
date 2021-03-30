968530556891882ff2c08b7202e9c2ede5731c4e
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2021-03-30 11:08:22 -0600

    for merge

--- social-media-react-app/src/components/Follow/Following/FollowingListItem.js
+++ social-media-react-app/src/components/Follow/Following/FollowingListItem.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import { Flex } from '../Follow.styles';
@@ -6,17 +7,39 @@ import { Link } from 'react-router-dom'
 
 
 const FollowingListItem = (props) => {
+    axios.defaults.headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+    }
     const [isFollowing, setIsFollowing] = useState(true);
+    const [error, setError] = useState(false);
 
     const handleFollow = () => {
-        setIsFollowing(true);
         //Api Call
+        function addFollow(user) {
+            if (user !== "") {
+                axios.get("https://localhost:5001/AddNewFollow/" + user)
+                    .then(res => setIsFollowing(true))
+                    .catch(err => setError(true) && console.log(err));
+            }
+        }
     }
     const handleUnFollow = () => {
-        setIsFollowing(false);
         //api call
+        function removeFollow(user) {
+            if (user !== "") {
+                axios.get("https://localhost:5001/RemoveFollow/" + user)
+                    .then(res => setIsFollowing(false))
+                    .catch(err => setError(true) && console.log(err));
+            }
+        }
+
     }
     var link = '/profile/'+ props.username;
+    if(error)
+    {
+        
+    }
     return (
         <Flex>
             <Avatar src={props.src} />
