fbea28689f2c1a898b598bf2fcfd06c2b5566832
Noris <45073527+JNoris@users.noreply.github.com>
2021-03-29 19:27:12 -0600

    Registration + Login, Data Validation  + Styling

--- social-media-react-app/src/App.js
+++ social-media-react-app/src/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, Switch } from "react-router";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
-import Settings2 from "./components/Settings/Settings2";
-import SignInSide from "./components/Login/SignInSide";
+import Settings from "./components/Settings/Settings";
 import SideNav from "./components/SideNav/SideNav";
 // import TopNav from "./components/TopNav/TopNav";
 import Chat from "./components/Chat/Chat";
@@ -27,15 +26,14 @@ function App() {
             <Route exact path="/add" component={AddPost} />
             <Route path="/profile/:id?" component={Profile} />
             <Route exact path="/follow" component={Follow} />
-            <Route exact path="/settings" component={Settings2} />
+            <Route exact path="/settings" component={Settings} />
             <Route exact path="/chat" component={Chat} />
             {/* <Route exact path='/TEST' component={TEST}/> */}
           </MainWrapper>
         </Flex>
       </Switch>
     );
-  }
-  else {
+  } else {
     return (
       <>
         <Switch>
--- social-media-react-app/src/components/Login/Register.js
+++ social-media-react-app/src/components/Login/Register.js
@@ -1,30 +1,66 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import {
+  Button,
+  TextField,
+  Avatar,
+  CssBaseline,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  makeStyles,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
-const Wrapper = styled.div`
-  position: fixed;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: calc(100vh - calc(100vh - 100%));
-  justify-content: center;
-  align-items: center;
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-  p {
-    display: block;
-  }
-`;
+const Register = () => {
+  const classes = useStyles();
 
-class Register extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    userName: "",
-    passWord: "",
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+
+  const handleUsername = (event) => {
+    setUserName(event.target.value);
+  };
+  const handlePassword = (event) => {
+    setPassWord(event.target.value);
   };
 
-  inputValidation = () => {
+  const handleFirstname = (event) => {
+    setFirstname(event.target.value);
+  };
+  const handleLastname = (event) => {
+    setLastname(event.target.value);
+  };
+
+  // Valah-day-shiun
+  function inputValidation() {
     let res = false;
 
     // First name, upper or lower case
@@ -36,86 +72,142 @@ class Register extends Component {
     // Password, 6-20 char and must have a number and a special character
     let passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
 
-    //   this.state.userName === usernameRegex &&
-    //   this.state.passWord === passwordRegex
     if (
-      this.state.userName != "" ||
-      usernameRegex.test(String(this.state.userName).toLowerCase())
+      firstNameRegex.test(firstName) &&
+      lastNameRegex.test(lastName) &&
+      usernameRegex.test(userName) &&
+      passwordRegex.test(passWord)
     ) {
       res = true;
     }
 
     return res;
-  };
+  }
 
-  handleRegisterButton = () => {
-    axios
-      .post(`https://localhost:5001/register`, {
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        userName: this.state.userName,
-        passWord: this.state.passWord,
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          this.props.history.push("/login");
-        }
-      });
-  };
-
-  handleKeypress = (e) => {
-    if (e.keyCode === 13 || e.charCode === 13) {
-      this.btn.click();
+  const handleRegisterButton = () => {
+    if (inputValidation()) {
+      axios
+        .post(`https://localhost:5001/register`, {
+          firstName: firstName,
+          lastName: lastName,
+          userName: userName,
+          passWord: passWord,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            this.props.history.push("/login");
+          }
+        })
+        .catch((e) => window.alert(e));
     } else {
-      console.log("");
+      // TODO: potentially remove
+      window.alert("Error");
     }
   };
 
-  render() {
-    return (
-      <Wrapper>
-        <p>First Name</p>
-        <input
-          type="text"
-          value={this.state.firstName}
-          onChange={(e) => {
-            this.setState({ firstName: e.target.value });
-          }}
-        />
-        <p>Last Name</p>
-        <input
-          type="text"
-          value={this.state.lastName}
-          onChange={(e) => {
-            this.setState({ lastName: e.target.value });
-          }}
-        />
-        <p>Username</p>
-        <input
-          type="text"
-          value={this.state.userName}
-          onChange={(e) => {
-            this.setState({ userName: e.target.value });
-          }}
-        />
-        <p>Password</p>
-        <input
-          type="text"
-          value={this.state.passWord}
-          onChange={(e) => {
-            this.setState({ passWord: e.target.value });
-          }}
-        />
+  // Enter button functionality
+  useEffect(() => {
+    const listener = (event) => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        console.log("Enter key was pressed. Run your function.");
+        handleRegisterButton();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
 
-        <input
-          style={{ marginTop: "1vh" }}
-          type="button"
-          onClick={this.handleRegisterButton}
-          value="Register"
-        />
-      </Wrapper>
-    );
-  }
-}
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                value={firstName}
+                onChange={handleFirstname}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                value={lastName}
+                onChange={handleLastname}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="userName"
+                autoComplete="username"
+                value={userName}
+                onChange={handleUsername}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={passWord}
+                onChange={handlePassword}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleRegisterButton}
+            value="Register"
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+};
 
 export default Register;
